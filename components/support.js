5c1613cd723542fa19b7e75a83caa3cbfc01df71
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-05-04 09:53:01 +0200

    added supported section

---
+++ components/support.js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+import styles from "../styles/navbar.module.scss";
+import { BsHouseFill } from "react-icons/bs";
+
+export const Support = () => {
+  const router = useRouter();
+
+  return <div></div>;
+};

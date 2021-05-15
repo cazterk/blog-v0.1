b56c5667a8f1d0463be049ec6521c88e9080d75c
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-05-15 15:55:16 +0200

    styled button and fixed button bug of unlimited adding prevalue

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { Navbar } from "../components/navbar";
 import { Heros } from "../components/heros";
 import { Footer } from "../components/footer";
 import { Support } from "../components/support";
-import Pagination from "../components/pagination";
 import styles from "../styles/home.module.scss";
 import imageUrlBuilder from "@sanity/image-url";
 import { useState, useEffect } from "react";
@@ -54,7 +53,17 @@ export default function Home({ posts, load }) {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const showMoreItems = () => {
-    setvisible((preValue) => preValue + 2);
+    if (visible < posts.length) {
+      setvisible((preValue) => preValue + 2);
+    } else {
+      setvisible((preValue) => preValue + 0);
+    }
+  };
+
+  const showLessItems = () => {
+    if (visible > 2) {
+      setvisible((preValue) => preValue - 2);
+    }
   };
 
   return (
@@ -87,14 +96,28 @@ export default function Home({ posts, load }) {
           ) : (
             <> No posts yet</>
           )}
+        </div>
+      </div>
 
-          <button onClick={showMoreItems}> Load More </button>
-          {/* <Pagination
+      <div className={styles.holder}>
+        <div className={styles.button} onClick={showLessItems}>
+          <p className={styles.btnText}> Less! </p>
+          {/* <div className={styles.btnTwo}>
+            <p className={styles.btnText2}>Load</p>
+          </div> */}
+        </div>
+
+        <div className={styles.button} onClick={showMoreItems}>
+          <p className={styles.btnText}>More! </p>
+          {/* <div className={styles.btnTwo}>
+            <p className={styles.btnText2}>Load</p>
+          </div> */}
+        </div>
+        {/* <Pagination
             postsPerPage={postsPerPage}
             totalPosts={posts.length}
             paginate={paginate}
           /> */}
-        </div>
       </div>
 
       <Support />

8e8b7a49f593f2433c592bd9bd2c7173a5ac5be0
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-05-19 17:29:17 +0200

    refactored code

--- components/heros.js
+++ components/heros.js
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import styles from "../styles/heros.module.scss";
+import { Subscribe } from "./subscribe";
 
 export const Heros = () => {
   const router = useRouter();
@@ -8,23 +9,12 @@ export const Heros = () => {
     <div>
       <div className={styles.heros}>
         <div className={styles.container}>
-          <h1>Cazterk's blog</h1>
-          <p className={styles.subtitle}>
+          <div className={styles.heros__hTitle}>cazterk's blog</div>
+          <p className={styles.heros__subtitle}>
             Your bite-sized blog, keeping it short and simple but informative
           </p>
 
-          <div className={styles.subscribe}>
-            <form action="#">
-              <div className={styles.subscribe__formBox}>
-                <input
-                  type="text"
-                  name="EmailAddress"
-                  placeholder="Enter your email"
-                />
-                <button type="submit">Subscribe</button>
-              </div>
-            </form>
-          </div>
+          <Subscribe />
         </div>
       </div>
     </div>

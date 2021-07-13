052fe00820b403fd69d5de4c8544ebbb444d8710
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2021-07-13 14:18:15 +0200

    added newsletter intergration with mailchimp

--- components/subscribe.js
+++ components/subscribe.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 import MailchimpSubscribe from "react-mailchimp-subscribe";
 import styles from "../styles/subscribe.module.scss";
 
 export const Subscribe = () => {
   const MAILCHIMP_URL = process.env.MAILCHIMP_URL;
+
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState("IDLE");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const subscribe = async () => {
+    setState("LOADING");
+    setErrorMessage(null);
+    try {
+      const response = await axios.post("/api/newsletter", { email });
+      setState("SUCCESS");
+    } catch (e) {
+      setErrorMessage(e.response.data.error);
+      setState("ERROR");
+    }
+  };
 
   return (
     <div>
@@ -13,10 +31,22 @@ export const Subscribe = () => {
             type="text"
             name="EmailAddress"
             placeholder="enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </form>
-        <button type="submit">Subscribe</button>
+        <button
+          type="submit"
+          type="button"
+          disabled={state === "LOADING"}
+          onClick={subscribe}
+        >
+          Subscribe
+        </button>
       </div>
+
+      {state === "ERROR" && <p>{errorMessage}</p>}
+      {state === "SUCCESS" && <p>Success!</p>}
     </div>
   );
 };

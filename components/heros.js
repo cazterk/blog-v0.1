648775224c62b35866f5f4127f6f96aa4684bd6f
cazterk <zcephas2@gmail.com>
2021-08-11 20:13:01 +0200

    ++more visual patches on all round and intro changes

--- components/heros.js
+++ components/heros.js
@@ -11,9 +11,9 @@ export const Heros = () => {
         <div className={styles.container}>
           <h1 className={styles.heros__hTitle}>welcome to the terklog</h1>
           <p className={styles.heros__subtitle}>
-            Your bite-sized blog on tech, software and more, keeping it short
-            and simple but informative, with 500 words give or take per post,
-            focuses on vitals
+            With the principle of continuous improvement, the terklog delivers
+            quality content that's ever getting better, subscribe to get the
+            latest updates{" "}
           </p>
 
           <Subscribe />
